bd99856fff09fde312a2ddf353f30223dd2a1b2f
Arjit Jain <thearjitjain@gmail.com>
2021-06-04 20:59:34 +0530

    Update memstream.py

--- memstream.py
+++ memstream.py
@@ -9,53 +9,36 @@ from sklearn import metrics
 import scipy.spatial as sp
 from torch.autograd import Variable
 import argparse
+import scipy.io
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', default='NSL')
+parser.add_argument('--dataset')
 parser.add_argument('--beta', type=float, default=1e-5)
 parser.add_argument('--dim', type=int, default=12)
-parser.add_argument("--dev", help="device", default="cpu")
+parser.add_argument("--dev", help="device", default="cuda:0")
 parser.add_argument("--epochs", type=int, help="number of epochs for ae", default=5000)
 parser.add_argument("--lr", type=float, help="learning rate", default=1e-2)
 parser.add_argument("--memlen", type=int, help="size of memory", default=512)
-parser.add_argument("--seed", type=int, help="size of memory", default=0)
+parser.add_argument("--seed", type=int, help="random seed", default=0)
 args = parser.parse_args()  
-torch.manual_seed(args.seed)
 
+torch.manual_seed(args.seed)
 nfile = None
 lfile = None
-if args.dataset == 'NSL':
-    nfile = './data/nsl.txt'
-    lfile = './data/nsllabel.txt'
-elif args.dataset == 'KDD': 
-    nfile = './data/kdd.txt'
-    lfile = './data/kddlabel.txt'
-elif args.dataset == 'UNSW': 
-    nfile = './data/unsw.txt'
-    lfile = './data/unswlabel.txt'
-elif args.dataset == 'DOS': 
-    nfile = './data/dos.txt'
-    lfile = './data/doslabel.txt'
-elif args.dataset == 'IDS': 
-    nfile = './data/iscx.csv'
-    lfile = './data/iscx_label.csv'
-
+df = scipy.io.loadmat('/home/siddharth/newdatasets/'+args.dataset)
 device = torch.device(args.dev)
 # device = 'cpu'
-
 class MemStream(nn.Module):
     def __init__(self, in_dim, params):
         super(MemStream, self).__init__()
         self.params = params
         self.in_dim = in_dim
-        self.out_dim = params['code_len']
+        self.out_dim = in_dim*2
         self.memory_len = params['memory_len']
         self.max_thres = torch.tensor(params['beta']).to(device)
         self.memory = torch.randn(self.memory_len, self.out_dim).to(device)
         self.mem_data = torch.randn(self.memory_len, self.in_dim).to(device)
-        self.mem_idx = torch.from_numpy(np.arange(self.memory_len))
         self.memory.requires_grad = False
         self.mem_data.requires_grad = False
-        self.mem_idx.requires_grad = False
         self.batch_size = params['memory_len']
         self.num_mem_update = 0
         self.encoder = nn.Sequential(
@@ -67,7 +50,6 @@ class MemStream(nn.Module):
         ).to(device)
         self.clock = 0
         self.last_update = -1
-        self.updates = []
         self.optimizer = torch.optim.Adam(self.parameters(), lr=params['lr'])
         self.loss_fn = nn.MSELoss()
         self.count = 0
@@ -84,18 +66,15 @@ class MemStream(nn.Module):
             loss = self.loss_fn(output, new)
             loss.backward()
             self.optimizer.step()
-        print("Autoencoder Loss", loss.item()) 
 
 
     def update_memory(self, output_loss, encoder_output, data):
         if output_loss <= self.max_thres:
-            least_used_pos = torch.argmin(self.mem_idx)
+            least_used_pos = self.count%self.memory_len
             self.memory[least_used_pos] = encoder_output
             self.mem_data[least_used_pos] = data
             self.mean, self.std = self.mem_data.mean(0), self.mem_data.std(0)    
-            self.mem_idx[least_used_pos] = self.count
             self.count += 1
-            self.num_mem_update += 1  
             return 1
         return 0    
     
@@ -117,53 +96,46 @@ class MemStream(nn.Module):
 #         loss_values = torch.mean(
 #             torch.nn.MSELoss(reduction='none')(mem_output, encoder_output), dim=1)
 #         for i in range(loss_values.shape[0]):
-        self.updates.append(self.update_memory(loss_values, encoder_output, x))
+        self.update_memory(loss_values, encoder_output, x)
         return loss_values
     
     
-numeric = torch.FloatTensor(np.loadtxt(nfile, delimiter = ','))
-labels = np.loadtxt(lfile, delimiter=',')
-if args.dataset == 'KDD':
-    labels = 1 - labels
-torch.manual_seed(args.seed)
-N = args.memlen
-params = {
-          'beta': args.beta, 'code_len': args.dim, 'memory_len': N, 'batch_size':1, 'lr':args.lr
-         }
+numeric = torch.FloatTensor(df['X'])
+labels = (df['y']).astype(float).reshape(-1)
+if args.memlen > numeric.shape[0]:
+    exit(0)
+for beta in [10, 1, 1e-1, 1e-2, 1e-3, 1e-4]:
+    args.beta = beta
+#     torch.manual_seed(args.seed)
+#     np.random.seed(args.seed)
+    N = args.memlen
+    params = {
+              'beta': args.beta, 'code_len': args.dim, 'memory_len': N, 'batch_size':1, 'lr':args.lr
+             }
 
-model = MemStream(numeric[0].shape[0],params).to(device)
+    model = MemStream(numeric[0].shape[0],params).to(device)
 
-batch_size = params['batch_size']
-print(args.dataset, args.beta, args.dim, args.memlen, args.lr, args.epochs)
-data_loader = DataLoader(numeric, batch_size=batch_size)
-init_data = numeric[labels == 0][:N].to(device)
-model.mem_data = init_data
-torch.set_grad_enabled(True)
-model.train_autoencoder(Variable(init_data).to(device), epochs=args.epochs)
-torch.set_grad_enabled(False)
-model.initialize_memory(Variable(init_data[:N]))
-# model.encoder.eval()
-err = []
-t = time.time()
-for data in data_loader:
-    output = model(data.to(device))
-    err.append(output)
-print("Time Taken", time.time() - t)        
-scores = np.array([i.cpu() for i in err])
-print("Number of Updates: ", model.num_mem_update)
-auc = metrics.roc_auc_score(labels, scores)
-count = int(np.sum(labels))
-preds = np.zeros_like(labels)
-indices = np.argsort(scores, axis=0)[::-1]
-preds[indices[:count]] = 1
-f1 = metrics.f1_score(labels, preds)
-print("F1", f1, "AUC", auc)
-print("Confusion Matrix", metrics.confusion_matrix(labels, preds))
-something = (1 - labels)*scores
-something = something[np.nonzero(1-labels)]
-normal = np.sort(something)
-something = labels*scores
-something = something[np.nonzero(labels)]
-anomaly = np.sort(something)
-print("Normal stats", np.median(normal), np.max(normal), np.min(normal), np.mean(normal))
-print("Anomaly stats", np.median(anomaly), np.max(anomaly), np.min(anomaly), np.mean(anomaly))
+    batch_size = params['batch_size']
+    print(args.dataset, args.beta, args.dim, args.memlen, args.lr, args.epochs)
+    data_loader = DataLoader(numeric, batch_size=batch_size)
+    init_data = numeric[labels == 0][:N].to(device)
+    model.mem_data = init_data
+    torch.set_grad_enabled(True)
+    model.train_autoencoder(Variable(init_data).to(device), epochs=args.epochs)
+    torch.set_grad_enabled(False)
+    model.initialize_memory(Variable(init_data[:N]))
+    # model.encoder.eval()
+    err = []
+    t = time.time()
+    for data in data_loader:
+        output = model(data.to(device))
+        err.append(output)
+    print("Time Taken", time.time() - t)        
+    scores = np.array([i.cpu() for i in err])
+    auc = metrics.roc_auc_score(labels, scores)
+    count = int(np.sum(labels))
+    preds = np.zeros_like(labels)
+    indices = np.argsort(scores, axis=0)[::-1]
+    preds[indices[:count]] = 1
+    f1 = metrics.f1_score(labels, preds)
+    print("F1", f1, "AUC", auc)
